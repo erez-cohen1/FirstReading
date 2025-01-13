075ce8ef802f124bff11b382a3277f970a7a3d48
erez1 <erez.cohen4@mail.huji.ac.il>
2025-01-13 21:01:44 +0200

    trying to make vercel use fresh redash data num 4

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     // Adding a cache-busting parameter (timestamp or random number)
