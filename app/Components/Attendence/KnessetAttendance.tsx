4182f4c2f097c34eac6baafde819485640fcb323
Itamar Yekutiel <yekutiel.itamar@gmail.com>
2025-01-28 11:41:59 +0200

    fix the full-Attendence list sticky

--- app/Components/Attendence/KnessetAttendance.tsx
+++ app/Components/Attendence/KnessetAttendance.tsx
@@ -68,7 +68,7 @@ const KnessetAttendance: React.FC<KnessetAttendanceProps> = ({date, isShrunk}) =
         <DisplayOptions mkData={mkData} displayOption={displayOption} setDisplayOption={setDisplayOption} />
         <AttendanceChart mkData={mkData} displayOption={displayOption} modalRef={modalRef} />
         {/* <NonMkChart mkData={mkData} govData={govData} displayOption={displayOption} modalRef={modalRef} /> */}
-        <div className={`Component-footer attendance ${showModal ? "modal-active" : ""}`}>
+        <div className={`Component-footer attendance ${showModal ? "modal-active" : ""} ${isShrunk ? "" : "footer-big-header-stick"}`}>
           <div className="footer-text">לרשימה המלאה</div>
           <i className={`arrow footer-arrow ${showModal ? "up" : "down"}`} onClick={() => handleToggleModal()}></i>
         </div>
