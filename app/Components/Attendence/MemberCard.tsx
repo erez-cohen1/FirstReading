2e502d8bb176bdcee3de9b5ecca64c057f9e4f2e
Itamar Yekutiel <yekutiel.itamar@gmail.com>
2025-01-28 11:13:23 +0200

    added initial info when clicking mk in the full list

--- app/Components/Attendence/MemberCard.tsx
+++ app/Components/Attendence/MemberCard.tsx
@@ -3,33 +3,18 @@ import { MkData } from "./MkData";
 
 interface MemberCardProps {
   mk: MkData;
+  onActivate: (mk: MkData) => void;
 }
 
-const MemberCard: React.FC<MemberCardProps> = ({ mk }) => {
-    const [flippedId, setFlippedId] = useState<number | null>(null);
-    
-    const handleFlip = (id: number) => {
-        setFlippedId(flippedId === id ? null : id); // Toggle the flip state
-      };
-      
+const MemberCard: React.FC<MemberCardProps> = ({ mk, onActivate }) => {
   return (
-    <div
-        className={`grid-item ${flippedId === mk.MkId ? "flipped" : ""}`}
-        key={mk.MkId}
-        onClick={() => handleFlip(mk.MkId)}
-    >
-        {flippedId === mk.MkId ? (
-        <div className="mk-info">
-            <div className="mk-name">{mk.Name}</div>
-            <div className="mk-name">{mk.FactionName}</div>
-            <div className="mk-name">{mk.Phone}</div>
-        </div>
-        ) : (
-          <>
-            <img src={mk.MkImage} alt={mk.Name} className={`mk-image ${mk.IsPresent ? "" : "grayscale"}`} />
-            <div className="mk-name">{mk.Name}</div>
-          </>
-        )}
+    <div className="grid-item" onClick={() => onActivate(mk)}>
+      <img
+        src={mk.MkImage}
+        alt={mk.Name}
+        className={`mk-image ${mk.IsPresent ? "" : "grayscale"}`}
+      />
+      <div className="mk-name">{mk.Name}</div>
     </div>
   );
 };
