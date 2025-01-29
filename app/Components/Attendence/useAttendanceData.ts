9a1828f61541b1cd2132256ac9cd4504255f1682
Itamar Yekutiel <yekutiel.itamar@gmail.com>
2025-01-29 09:56:25 +0200

    fix bug

--- app/Components/Attendence/useAttendanceData.ts
+++ app/Components/Attendence/useAttendanceData.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { MkData } from "./MkData";
 import { govData } from "./posData";
 import mkDetails from "./mk_Data281.json";
-import govData1 from "./govermentPos.json";
+//import govData1 from "./govermentPos.json";
 
 export const useAttendanceData = (): [MkData[], boolean, string | null] => {
   const [data, setData] = useState<MkData[]>([]);
@@ -83,7 +83,7 @@ export const useAttendanceDataFromFile = (date: Date): [MkData[], boolean, strin
           const response = await fetch("https://knesset.gov.il/WebSiteApi/knessetapi/MkLobby/GetMkPresent?lang=he");
           if (!response.ok) throw new Error(`Error fetching attendance data: ${response.status}`);
 
-//           const attendanceMapping: { MkId: number, IsPresent: boolean }[] = await response.json();
+          const attendanceMapping: { MkId: number, IsPresent: boolean }[] = await response.json();
           
           // Step 3: Update local data to reflect attendance status
           updatedData = localData.map((mk) => {
@@ -118,18 +118,18 @@ export const useAttendanceDataFromFile = (date: Date): [MkData[], boolean, strin
 
         
 
-//         setData(updatedData);
-//         //setGovData(updatedGovData);
-//       } catch (err) {
-//         setError((err as Error).message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
+        setData(updatedData);
+        //setGovData(updatedGovData);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-//     fetchAttendanceData();
-//   }, [date]);
+    fetchAttendanceData();
+  }, [date]);
 
-//   return [data, loading, error];
-// };
+  return [data, loading, error];
+};
 
